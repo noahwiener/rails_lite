ea922a742e87873ecce1bb0e5cc53aeabc5f5d35
noahwiener <noah.wiener@gmail.com>
2015-09-22 13:45:59 -0700

    started rails lite

---
+++ bin/p01_basic_server.rb
@@ -0,0 +1,19 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new(:Port => 3000)
+
+server.mount_proc("/") do |request, response|
+  response.content_type = "text/text"
+  response.body = "#{request.path}"
+end
+
+trap('INT') do
+  server.shutdown
+end
+
+server.start
+
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
+# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
---
+++ lib/phase5/params.rb
@@ -0,0 +1,39 @@
+require 'uri'
+
+module Phase5
+  class Params
+    # use your initialize to merge params from
+    # 1. query string
+    # 2. post body
+    # 3. route params
+    #
+    # You haven't done routing yet; but assume route params will be
+    # passed in as a hash to `Params.new` as below:
+    def initialize(req, route_params = {})
+    end
+
+    def [](key)
+    end
+
+    # this will be useful if we want to `puts params` in the server log
+    def to_s
+      @params.to_s
+    end
+
+    class AttributeNotFoundError < ArgumentError; end;
+
+    private
+    # this should return deeply nested hash
+    # argument format
+    # user[address][street]=main&user[address][zip]=89436
+    # should return
+    # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
+    def parse_www_encoded_form(www_encoded_form)
+    end
+
+    # this should return an array
+    # user[address][street] should return ['user', 'address', 'street']
+    def parse_key(key)
+    end
+  end
+end
